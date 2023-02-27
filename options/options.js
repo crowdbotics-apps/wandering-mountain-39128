263bda7ae93c8ddd9d34ce25c6a743bb85e8c890
Crowdbotics <team@crowdbotics.com>
2023-02-27 13:35:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-mountain-39128",
+  url: "https://wandering-mountain-39128.botics.co",
+  api: "https://wandering-mountain-39128.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
